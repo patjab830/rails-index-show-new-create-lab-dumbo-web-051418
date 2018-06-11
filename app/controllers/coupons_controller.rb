d7fa8e4ca487a9c2c32f4d2a6be0e04e4c251ea5
github username <github email address>
2018-06-10 23:44:12 -0400

    implemented coupons from scratch

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,25 @@
+require 'pry'
+class CouponsController < ApplicationController
+
+  def index
+    @coupons = Coupon.all
+  end
+
+  def new
+    @coupon = Coupon.new
+  end
+
+  def create
+    @coupon = Coupon.create(coupon_params)
+    redirect_to coupon_url(@coupon)
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  private
+  def coupon_params
+    params.require(:coupon).permit(:coupon_code, :store)
+  end
+end
